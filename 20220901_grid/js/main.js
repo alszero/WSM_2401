6930ff849f7c508e19d95e34ed73d37a4dcc52e8
alszero <alszero05@gmail.com>
2022-09-15 15:10:30 +0900

    js 날짜구하기

---
+++ 20220901_grid/js/main.js
@@ -0,0 +1,13 @@
+// 현재 년, 월, 일 알아내자
+let today = new Date();
+
+year = today.getFullYear();
+month = today.getMonth(); //0~11
+date = today.getDate();
+day = today.getDay();   //0:일, 1:월, 2:화, 3:수, 4:목, 5:금, 6:토
+// 현재 월 마지막날?
+let lastDate = new Date(year, month + 1, 0).getDate();
+// 현재 월 1일은 무슨 요일?
+let firstDate = new Date(year, month, 1).getDay();
+console.log(`${year}년 ${month}월 ${date}일 ${firstDate}, ${lastDate}`);
+
