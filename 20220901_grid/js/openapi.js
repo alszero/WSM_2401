6ca29950938eeda531ee950353f61d31c713a33d
alszero <alszero05@gmail.com>
2022-10-11 15:40:56 +0900

    start AJAX

--- 20220901_grid/js/openapi.js
+++ 20220901_grid/js/openapi.js
@@ -12,9 +12,16 @@
 //MLSV_FROM_YMD: 급식시작일자
 //MLSV_TO_YMD: 급식종료일자
 
-https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010569&MMEAL_SC_CODE=2&MLSV_YMD=20220927&Type=json
+//https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010569&MMEAL_SC_CODE=2&MLSV_YMD=20221011&Type=json
 const KEY = "f320ae33a34e4f49adf427bf849edbcd";
 const ATPT_OFCDC_SC_CODE = "B10";   //서울특별시교육청
 const SD_SCHUL_CODE = "7010569";    //미림여자정보과학고등학교
 let MMEAL_SC_CODE = 2; //중식
-let MLSV_YMD = "20220927";  //YYYYMMDD
+let MLSV_YMD = "20221011";  //YYYYMMDD
+let url = `https://open.neis.go.kr/hub/mealServiceDietInfo?`
+        +  `KEY=${KEY}`
+        +  `ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`
+        +  `SD_SCHUL_CODE=${SD_SCHUL_CODE}`
+        +  `MLSV_YMD=${MLSV_YMD}`
+        +  `MMEAL_SC_CODE=${MMEAL_SC_CODE}`;
+console.log(url);
