c02d5f7111f6b790786cb8cf698b18afe4f05445
alszero <alszero05@gmail.com>
2022-10-20 14:35:03 +0900

    AJAX 완성

--- 20220901_grid/js/openapi.js
+++ 20220901_grid/js/openapi.js
@@ -42,12 +42,63 @@ const handler = (e) =>{
         console.log(MLSV_YMD);
         let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`
                 +  `?KEY=${KEY}`
+                +  `&Type=json`
                 +  `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`
                 +  `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`
                 +   `&MLSV_YMD=${MLSV_YMD}`;
                 //+   `&MMEAL_SC_CODE=${MMEAL_SC_CODE}`;
-                console.log(url);
-        //urlToJSON(url);
+        console.log(url);
+        urlToJSON(url);
+}
+const urlToJSON = (url) => {
+        //XMLHttpRequest 객체 만들자
+        let xhr = new XMLHttpRequest();
+
+        //callback
+        xhr.onreadystatechange = () => {
+                if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
+                        //success
+                        //console.log("성공" + xhr.response);
+                        showMenu(xhr.response);
+                }else{
+                        //fail
+                        //console.log(xhr.status);
+                }
+        }
+
+        //요청을 보낼 방식 정하자
+        xhr.open("GET", url, true);
+        
+        //요청하자
+        xhr.send();
+
+        //json 받아서 HTML 조식, 중식, 석식에 보여주자
+        const showMenu = (jsonString) => {
+                console.log(jsonString);
+                //jsonString -> json
+                let json = JSON.parse(jsonString);         //"{'key': 'value'}"" -> {'key': 'value'}
+                try{
+                        if(json['mealServiceDietInfo'][0]['head'][1]['RESULT']['CODE']=='INFO-000'){
+                                //응답이 제대로 왔으면
+                                //json -> HTML
+                                breakfast.innerHTML = json['mealServiceDietInfo'][1]['row'][0]['DDISH_NM']; //조식
+                                lunch.innerHTML = json['mealServiceDietInfo'][1]['row'][1]['DDISH_NM']; //중식
+                                dinner.innerHTML = json['mealServiceDietInfo'][1]['row'][2]['DDISH_NM']; //석식
+                        }else{
+                                //응답이 이상하면
+                                //없음 표시하자
+                                breakfast.innerHTML = " 없응";
+                                lunch.innerHTML = " 없응";
+                                dinner.innerHTML = " 없응";
+                        } 
+                }catch{ //문제가 생기면 {'RESULT':}
+                        breakfast.innerHTML = " 없응";
+                        lunch.innerHTML = " 없응";
+                        dinner.innerHTML = " 없응";  
+                }      
+        }
+        
+
 }
 for(let gridItem of gridItems){
         gridItem.onmouseover= handler;
